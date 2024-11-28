fdce1950384e26cb24c99fde3de689066a132f16
tutek1 <ondrej@kyzr.cz>
2024-11-28 18:15:23 +0100

    Implemented MCTSNode

--- Chess-AI-main/Assets/Scripts/Core/AI/MCTSNode.cs
+++ Chess-AI-main/Assets/Scripts/Core/AI/MCTSNode.cs
@@ -2,10 +2,43 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using UnityEngine;
 
     public class MCTSNode
     {
+        public Board state;
+        public MCTSNode parent;
+        public List<MCTSNode> children = new List<MCTSNode>();
+        public List<Move> allPossibleMoves = new List<Move>();
+        public int lastExpandedMoveIdx = -1;
+        public Move initialMove;    // The initial move that lead to this state 
+        public int numVisits = 0;
+        public float numWins = 0;
+        public float UCTValue = 0;
+        public bool isEnemyTurn;
 
+        public MCTSNode(Board board, Move initialMove, bool isEnemyTurn)
+        {
+            this.state = board;
+            this.initialMove = initialMove;
+            this.isEnemyTurn = isEnemyTurn;
+        }
+
+        public void Backpropagate(float result)
+        {
+            numVisits++;
+            numWins += isEnemyTurn? -result : result;
+
+            if (parent != null)
+            {
+                parent.Backpropagate(result);
+            }
+        }
+
+        public MCTSNode SelectBestChild()
+        {
+            return children.OrderByDescending(child => child.UCTValue).FirstOrDefault();
+        }
     }
 }
